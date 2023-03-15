00ae34faa7bfde1abfb8616994ed96a2665febdd
Austin Ofor <30274314+kwadoskii@users.noreply.github.com>
2023-03-15 12:02:21 +0100

    Update page.jsx

--- app/movie/[movie_id]/page.jsx
+++ app/movie/[movie_id]/page.jsx
@@ -11,26 +11,28 @@ export default async function page({ params }) {
   ).then((data) => data.json());
 
   return (
-    <div className="flex flex-col max-w-7xl px-10 min-h-screen py-5 mx-auto gap-5 bg-slate-900">
-      <Image
-        src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-        priority
-        width={1000}
-        height={1000}
-        className="rounded-lg"
-      />
+    <div className="min-h-screen px-10 py-5 bg-slate-900 text-white">
+      <div className="flex flex-col max-w-7xl mx-auto gap-5">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+          priority
+          width={1000}
+          height={1000}
+          className="rounded-lg"
+        />
 
-      <div className="">
-        <h3 className="text-4xl">{movie.title}</h3>
-        <p className="text-sm">Released: {movie.release_date.substr(0, 4)}</p>
-      </div>
-      <p className="text-lg leading-normal md:w-4/5 lg:w-3/5">{movie.overview}</p>
-      <p className="text-sm">
-        Runtime:
-        <span className="text-base">{movie.runtime} minutes</span>
-      </p>
-      <div className="flex bg-green-600 text-white w-fit p-3 rounded-md py-1.5 text-xs">
-        <p>{movie.status}</p>
+        <div className="">
+          <h3 className="text-4xl">{movie.title}</h3>
+          <p className="text-sm">Released: {movie.release_date.substr(0, 4)}</p>
+        </div>
+        <p className="text-lg leading-normal md:w-4/5 lg:w-3/5">{movie.overview}</p>
+        <p className="text-sm">
+          Runtime:
+          <span className="text-base">{movie.runtime} minutes</span>
+        </p>
+        <div className="flex bg-green-600 text-white w-fit p-3 rounded-md py-1.5 text-xs">
+          <p>{movie.status}</p>
+        </div>
       </div>
     </div>
   );
